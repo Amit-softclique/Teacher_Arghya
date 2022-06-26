d413bfba8abb09fff9b91c72b91588926b28aa5c
Amit-softclique <royamit833@gmail.com>
2022-06-26 21:52:55 +0530

    1st test commit

--- components/EditScreenInfo.tsx
+++ components/EditScreenInfo.tsx
@@ -13,7 +13,7 @@ export default function EditScreenInfo({ path }: { path: string }) {
           style={styles.getStartedText}
           lightColor="rgba(0,0,0,0.8)"
           darkColor="rgba(255,255,255,0.8)">
-          Open up the code for this screen:
+          Open up the code for this screen: Hello Teacher!!
         </Text>
 
         <View
@@ -27,14 +27,14 @@ export default function EditScreenInfo({ path }: { path: string }) {
           style={styles.getStartedText}
           lightColor="rgba(0,0,0,0.8)"
           darkColor="rgba(255,255,255,0.8)">
-          Change any of the text, save the file, and your app will automatically update.
+          Change any of the text, save the file, and your app will automatically update. Hello Teacher!!
         </Text>
       </View>
 
       <View style={styles.helpContainer}>
         <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
           <Text style={styles.helpLinkText} lightColor={Colors.light.tint}>
-            Tap here if your app doesn't automatically update after making changes
+            Tap here if your app doesn't automatically update after making changes. Hello Teacher!!
           </Text>
         </TouchableOpacity>
       </View>
